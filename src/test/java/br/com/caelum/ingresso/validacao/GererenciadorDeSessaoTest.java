519a33f8a5795550a82028b910182c5cebfa2c5d
Adacir Mendes <adacirmendes@ig.com.br>
2018-10-15 19:41:59 -0300

    Inclusão de preço nas classes filme, sala e sessao

--- src/test/java/br/com/caelum/ingresso/validacao/GererenciadorDeSessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/validacao/GererenciadorDeSessaoTest.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.validacao;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -24,8 +25,8 @@ public class GererenciadorDeSessaoTest {
 	@Before
 	public void preparaSessoes(){
 		
-		this.rogueOne = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI");
-		this.sala3D = new Sala("Sala 3D");
+		this.rogueOne = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", BigDecimal.ONE);
+		this.sala3D = new Sala("Sala 3D", BigDecimal.TEN);
 		
 		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueOne, sala3D);
 		this.sessaoDasTreze = new Sessao(LocalTime.parse("13:00:00"), rogueOne, sala3D);
@@ -60,7 +61,7 @@ public class GererenciadorDeSessaoTest {
 	public void garanteQueDevePermitirUmaInsercaoEntreDoisFilmes() {
 		List<Sessao> sessoes = Arrays.asList(sessaoDasDez, sessaoDasDezoito);
 		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
-		Assert.assertFalse(gerenciador.cabe(sessaoDasTreze));
+		Assert.assertTrue(gerenciador.cabe(sessaoDasTreze));
 	}
 
 }
